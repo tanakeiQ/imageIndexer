6b0f2f771699a28b39c82982c8574d46f41359aa
tanakeiQ <tanakei@suitehearts.co>
2018-02-04 22:05:29 +0900

    Save csv to mapping data.
    - TODO: get file pixel.

--- main.py
+++ main.py
@@ -10,28 +10,40 @@
 # 
 # Created by tanakeiQ<tanakei@suitehearts.co>
 
-import os, sys, uuid, csv
+import os, sys, uuid, csv, atexit
 from distutils.spawn import find_executable
 
 # meta resources
+filemap=[]
 
 # ImageMagick
 image_magick_path=find_executable('convert')
 
 # Target resources
+# image resources directory
 input_dir='%s/resources' % (os.getcwd())
-output_ext='.png'
-output_thumb_dir='%s/output' % (os.getcwd())
+# output file extension(don't forget dor(.))
+output_ext='png'
+# output thumbnail directory
+output_thumb_dir='%s/output/thumb' % (os.getcwd())
+# ouput csv directory
+output_csv_path='%s/output/_map.csv' % (os.getcwd())
+# If applied script into symbolic link, set `True`
 isEnableSym=False
-avairable_formats=['.ai','.eps','.psd','.png','.jpg','.jpeg','.bmp','.ico','.ttf','.pdf']
+# Is customize script, modify avairable file format.
+avairable_formats=['ai','eps','psd','png','jpg','jpeg','bmp','ico','ttf','pdf']
 
-# Convert General Image
+### Convert General Image
+# thumbnail width
 width=300
+# thumbnail height
 height=300
+# thumbnail quality
 quality=80
+# thumbnail density
 density=300
 
-cmd_common_output=' "%s/%s_%s%s"'
+cmd_common_output=' "%s/%s"'
 cmd_common='%s "%s" -resize %dx%d -quality %d %s'
 
 # Convert Extension for Layer Image
@@ -49,26 +61,36 @@ NOPQRSTUVWXYZ
 cmd_font='%s -font "%s" -pointsize %s label:"%s" -resize %dx%d -quality %d %s'
 
 
+def writeCsv():
+	with open(output_csv_path, 'w') as f:
+		writer = csv.writer(f, lineterminator='\n')
+		writer.writerows(filemap)
+
 def convert(path, filename):
 	cmd=''
 	name, ext = os.path.splitext(filename)
+	ext = ext.lstrip('.')
 	if not ext in avairable_formats:
 		print('💦  file extension `%s` is not support: %s' % (ext, path))
 		return
 
-	output=cmd_common_output % (output_thumb_dir, uuid.uuid4(), name, output_ext)
+	output_filename="%s_%s.%s" % (uuid.uuid4(), name, output_ext)
+	output=cmd_common_output % (output_thumb_dir, output_filename)
 
-	if ext in {'.psd', '.ico', '.ai', '.eps'}:
+	if ext in {'psd', 'ico', 'ai', 'eps'}:
 		cmd = cmd_layer % (image_magick_path, path, density, width, height, quality, output)
-	elif ext in {'.pdf'}:
+	elif ext in {'pdf'}:
 		cmd = cmd_layer % (image_magick_path, path, density, width, height, quality, output)
-	elif ext in {'.ttf'}:
+	elif ext in {'ttf'}:
 		cmd = cmd_font % (image_magick_path, path, font_pointsize, font_label, width, height, quality, output)
 	else:
 		cmd = cmd_common % (image_magick_path, path, width, height, quality, output)
 	try:
 		# print(cmd)
 		os.system(cmd)
+		# Filename, File path, Extension, Thumbnail path, File size, width, height
+		data=[name, ext, path, output_filename,os.path.getsize(path), 0, 0]
+		filemap.append(data)
 	except:	
 		print('❌  Filed to convert: %s' % (path))
 
@@ -105,4 +127,5 @@ if __name__ == '__main__':
 		if not os.path.exists(input_dir):
 			sys.stderr.write('❌  No such directory: %s')
 		print('Use convert<%s>' % (image_magick_path))
+		atexit.register(writeCsv)
 		rSearch(input_dir)
