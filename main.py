c6cbb758317ab3de3c905aada7d643c021e4632a
tanakeiQ <tanakei@suitehearts.co>
2018-02-06 03:15:13 +0900

    formated PEP8

--- main.py
+++ main.py
@@ -1,138 +1,149 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # ### ImageIndexer
-# 
+#
 # This script required below libraries.
-# 
+#
 # - ImageMagick
 # - ghostscript(For AI, EPS, PDF)
-# 
+#
 # Created by tanakeiQ<tanakei@suitehearts.co>
 
-import os, sys, uuid, atexit, json
+import os
+import sys
+import uuid
+import atexit
+import json
 from distutils.spawn import find_executable
 
 # meta resources
-filemap=[]
+filemap = []
 
 # ImageMagick
-image_magick_path=find_executable('convert')
+image_magick_path = find_executable('convert')
 
 # Target resources
 # image resources directory
-input_dir='%s/debug/resources' % (os.getcwd())
+input_dir = '%s/debug/resources' % (os.getcwd())
 # output file extension(don't forget dor(.))
-output_ext='png'
+output_ext = 'png'
 # output thumbnail directory
-output_thumb_dir='%s/debug/output/thumb' % (os.getcwd())
+output_thumb_dir = '%s/debug/output/thumb' % (os.getcwd())
 # ouput json directory
-output_json_path='%s/debug/output/_map.json' % (os.getcwd())
+output_json_path = '%s/debug/output/_map.json' % (os.getcwd())
 # If applied script into symbolic link, set `True`
-isEnableSym=False
+isEnableSym = False
 # Is customize script, modify avairable file format.
-avairable_formats=['ai','eps','psd','png','jpg','jpeg','bmp','ico','ttf','pdf']
+avairable_formats = ['ai', 'eps', 'psd', 'png',
+                     'jpg', 'jpeg', 'bmp', 'ico', 'ttf', 'pdf']
 
-### Convert General Image
+# Convert General Image
 # thumbnail width
-width=300
+width = 300
 # thumbnail height
-height=300
+height = 300
 # thumbnail quality
-quality=80
+quality = 80
 # thumbnail density
-density=300
+density = 300
 
-cmd_common_output=' "%s/%s"'
-cmd_common='%s "%s" -resize %dx%d -quality %d %s'
+cmd_common_output = ' "%s/%s"'
+cmd_common = '%s "%s" -resize %dx%d -quality %d %s'
 
 # Convert Extension for Layer Image
-cmd_layer='%s "%s[0]" -density %d -resize %dx%d -quality %d -colorspace CMYK %s'
+cmd_layer = '%s "%s[0]" -density %d -resize %dx%d -quality %d -colorspace CMYK %s'
 
 # Convert Extension for Font
-font_pointsize=100
-font_label='''
+font_pointsize = 100
+font_label = '''
 abcdefghijklmnopqrstuvwxyz
 ABCDEFGHIJKLM
 NOPQRSTUVWXYZ
 これはひらがな
 コレハカタカナ
 此れは漢字'''
-cmd_font='%s -font "%s" -pointsize %s label:"%s" -resize %dx%d -quality %d %s'
+cmd_font = '%s -font "%s" -pointsize %s label:"%s" -resize %dx%d -quality %d %s'
 
 
 def writeCsv():
-	with open(output_json_path, 'w') as f:
-		json.dump(filemap, f)
+    with open(output_json_path, 'w') as f:
+        json.dump(filemap, f)
+
 
 def convert(path, filename):
-	cmd=''
-	name, ext = os.path.splitext(filename)
-	ext = ext.lstrip('.')
-	if not ext in avairable_formats:
-		print('💦  file extension `%s` is not support: %s' % (ext, path))
-		return
+    cmd = ''
+    name, ext = os.path.splitext(filename)
+    ext = ext.lstrip('.')
+    if not ext in avairable_formats:
+        print('💦  file extension `%s` is not support: %s' % (ext, path))
+        return
 
-	output_filename="%s_%s.%s" % (uuid.uuid4(), name, output_ext)
-	output=cmd_common_output % (output_thumb_dir, output_filename)
+    output_filename = "%s_%s.%s" % (uuid.uuid4(), name, output_ext)
+    output = cmd_common_output % (output_thumb_dir, output_filename)
 
-	if ext in {'psd', 'ico', 'ai', 'eps'}:
-		cmd = cmd_layer % (image_magick_path, path, density, width, height, quality, output)
-	elif ext in {'pdf'}:
-		cmd = cmd_layer % (image_magick_path, path, density, width, height, quality, output)
-	elif ext in {'ttf'}:
-		cmd = cmd_font % (image_magick_path, path, font_pointsize, font_label, width, height, quality, output)
-	else:
-		cmd = cmd_common % (image_magick_path, path, width, height, quality, output)
-	try:
-		# print(cmd)
-		os.system(cmd)
-		# Filename, File path, Extension, Thumbnail path, File size, width, height
-		data= {
-			"name": name, 
-			"ext": ext, 
-			"path": path,
-			"filename": output_filename,
-			"size": os.path.getsize(path),
-			"w": 0,
-			"h": 0
-		}
-		filemap.append(data)
-	except:	
-		print('❌  Filed to convert: %s' % (path))
+    if ext in {'psd', 'ico', 'ai', 'eps'}:
+        cmd = cmd_layer % (image_magick_path, path, density,
+                           width, height, quality, output)
+    elif ext in {'pdf'}:
+        cmd = cmd_layer % (image_magick_path, path, density,
+                           width, height, quality, output)
+    elif ext in {'ttf'}:
+        cmd = cmd_font % (image_magick_path, path, font_pointsize,
+                          font_label, width, height, quality, output)
+    else:
+        cmd = cmd_common % (image_magick_path, path,
+                            width, height, quality, output)
+    try:
+        # print(cmd)
+        os.system(cmd)
+        # Filename, File path, Extension, Thumbnail path, File size, width,
+        # height
+        data = {
+            "name": name,
+            "ext": ext,
+            "path": path,
+            "filename": output_filename,
+            "size": os.path.getsize(path),
+            "w": 0,
+            "h": 0
+        }
+        filemap.append(data)
+    except:
+        print('❌  Filed to convert: %s' % (path))
 
 
 def rSearch(path):
-	filenames = os.listdir(path)
-	for filename in filenames:
-		fullpath = os.path.join(path, filename)
-		if os.path.islink(fullpath):
-			if isEnableSym:
-				rSearch(fullpath)
-			else:
-				print('🔗  Sym link is ignored: %s' % (fullpath))
-		elif os.path.isdir(fullpath):
-			rSearch(fullpath)
-		elif os.path.isfile(fullpath):
-			convert(fullpath, filename)
-		else:
-			print('💦  This file format not support: %s' % (fullpath))	
+    filenames = os.listdir(path)
+    for filename in filenames:
+        fullpath = os.path.join(path, filename)
+        if os.path.islink(fullpath):
+            if isEnableSym:
+                rSearch(fullpath)
+            else:
+                print('🔗  Sym link is ignored: %s' % (fullpath))
+        elif os.path.isdir(fullpath):
+            rSearch(fullpath)
+        elif os.path.isfile(fullpath):
+            convert(fullpath, filename)
+        else:
+            print('💦  This file format not support: %s' % (fullpath))
 
 
 if __name__ == '__main__':
-	if not image_magick_path:
-		sys.stderr.write('''
+    if not image_magick_path:
+        sys.stderr.write('''
 	❌  ImageMagick not installed
 	❌  Please install before. 
 	❌  
 	❌  https://www.imagemagick.org/script/download.php
 
 ''')
-	else:
-		if not os.path.exists(output_thumb_dir):
-			os.makedirs(output_thumb_dir)
-		if not os.path.exists(input_dir):
-			sys.stderr.write('❌  No such directory: %s')
-		print('Use convert<%s>' % (image_magick_path))
-		atexit.register(writeCsv)
-		rSearch(input_dir)
+    else:
+        if not os.path.exists(output_thumb_dir):
+            os.makedirs(output_thumb_dir)
+        if not os.path.exists(input_dir):
+            sys.stderr.write('❌  No such directory: %s')
+        print('Use convert<%s>' % (image_magick_path))
+        atexit.register(writeCsv)
+        rSearch(input_dir)
--- server.py
+++ server.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Created by tanakeiQ<tanakei@suitehearts.co>
-# 
-import json, bottle
+#
+import json
+import bottle
 from bottle import run, route, template, static_file
 
 app = bottle.default_app()
 app.config.load_config([
-	'config/resource.ini',
-	'config/server.ini'
+    'config/resource.ini',
+    'config/server.ini'
 ])
 
 print(app.config)
 
+
 @route('/')
 def index():
-	f = open(app.config['resource.map_path'], 'r')
-	reader =  json.load(f)
-	data = "{}".format(json.dumps(reader))
-	f.close()
-	return template('index',  {'data': data})
+    f = open(app.config['resource.map_path'], 'r')
+    reader = json.load(f)
+    data = "{}".format(json.dumps(reader))
+    f.close()
+    return template('index',  {'data': data})
+
 
 @route('/static/thumb/<filename>')
 def server_static(filename):
     return static_file(filename, root=app.config['resource.static_path'])
 
 
-run(host=app.config['server.host'], port=app.config['server.port'], debug=app.config['server.is_debug'])
+run(host=app.config['server.host'], port=app.config[
+    'server.port'], debug=app.config['server.is_debug'])
